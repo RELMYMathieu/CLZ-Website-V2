b5013947153629bc39ced8b523e7b703cafb0838
HOUA <ninjacowzx@gmail.com>
2023-03-21 12:34:59 +0100

    added dedicated js file, added scroll arrows

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function scrollToNextSection(isRecentLaunch) {
+    if (isRecentLaunch) {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+    } else {
+        document.querySelector('.recent-launch').scrollIntoView({ behavior: 'smooth' });
+    }
+}
