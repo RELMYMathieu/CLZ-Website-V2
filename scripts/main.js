1c14f50c6a95bab1bf5af1c878ab175957a24bc3
HOUA <ninjacowzx@gmail.com>
2023-03-24 19:41:18 +0100

    rollback due to broken code

--- scripts/main.js
+++ scripts/main.js
@@ -1,69 +1,78 @@
 function scrollToNextSection(isRecentLaunch) {
-  if (isRecentLaunch) {
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: 'smooth'
-    });
-  } else {
-    document.querySelector('.recent-launch').scrollIntoView({ behavior: 'smooth' });
-  }
+    if (isRecentLaunch) {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+    } else {
+        document.querySelector('.recent-launch').scrollIntoView({ behavior: 'smooth' });
+    }
 }
 
 function scrollToNextTeamSection() {
-  const csuiteSection = document.querySelector('.csuite');
-  csuiteSection.scrollIntoView({ behavior: 'smooth' });
-}
-
-function scrollToNextJobSection() {
-  const csuiteSection = document.querySelector('.job-listing');
-  csuiteSection.scrollIntoView({ behavior: 'smooth' });
-}
-
-function updateWorkExperienceFields() {
-  const numCompanies = document.getElementById('num-companies').value;
-  const workExperienceFields = document.getElementById('work-experience-fields');
-
-  workExperienceFields.innerHTML = '';
-
-  for (let i = 0; i < numCompanies; i++) {
-    const companyField = `
-      <h3>Company ${i + 1}</h3>
-      <label for="company${i + 1}">Company:</label>
-      <input type="text" id="company${i + 1}" name="company${i + 1}"><br>
-
-      <label for="position${i + 1}">Position:</label>
-      <input type="text" id="position${i + 1}" name="position${i + 1}"><br>
-
-      <label for="duration${i + 1}">Duration:</label>
-      <input type="text" id="duration${i + 1}" name="duration${i + 1}"><br>
-    `;
-
-    workExperienceFields.innerHTML += companyField;
+    const csuiteSection = document.querySelector('.csuite');
+    csuiteSection.scrollIntoView({ behavior: 'smooth' });
   }
-}
 
-document.getElementById('num-companies').addEventListener('change', updateWorkExperienceFields);
+  function scrollToNextJobSection() {
+    const csuiteSection = document.querySelector('.job-listing');
+    csuiteSection.scrollIntoView({ behavior: 'smooth' });
+  }
 
-document.getElementById('EngineerForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-  const formData = new FormData(event.target);
+  window.addEventListener('load', function() {
+    document.body.style.opacity = 1;
+  });
 
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://clzwebserver.jklninjacowz.repl.co', true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        alert('Email sent successfully!');
-      } else {
-        alert('An error occurred while sending the email');
-      }
+window.addEventListener('load', function() {
+    document.body.style.opacity = 1;
+  });
+
+  function updateWorkExperienceFields() {
+    const numCompanies = document.getElementById('num-companies').value;
+    const workExperienceFields = document.getElementById('work-experience-fields');
+
+    workExperienceFields.innerHTML = '';
+
+    for (let i = 0; i < numCompanies; i++) {
+      const companyField = `
+        <h3>Company ${i + 1}</h3>
+        <label for="company${i + 1}">Company:</label>
+        <input type="text" id="company${i + 1}" name="company${i + 1}"><br>
+
+        <label for="position${i + 1}">Position:</label>
+        <input type="text" id="position${i + 1}" name="position${i + 1}"><br>
+
+        <label for="duration${i + 1}">Duration:</label>
+        <input type="text" id="duration${i + 1}" name="duration${i + 1}"><br>
+      `;
+
+      workExperienceFields.innerHTML += companyField;
     }
-  };
-  document.getElementById("EngineerForm").style.display = "none";
-  document.getElementById("thankYouMessage").style.display = "block";
-  xhr.send(formData);
-  startCountdown();
-});
+  }
+
+  document.getElementById('num-companies').addEventListener('change', updateWorkExperienceFields);
+
+  document.getElementById('EngineerForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+  
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://clzwebserver.jklninjacowz.repl.co', true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          alert('Email sent successfully!');
+        } else {
+          alert('An error occurred while sending the email');
+        }
+      }
+    };
+    document.getElementById("EngineerForm").style.display = "none";
+    document.getElementById("thankYouMessage").style.display = "block";
+    xhr.send(formData);
+    startCountdown();
+  });
+  
 
 function startCountdown() {
   let countdown = document.getElementById("countdown");
@@ -80,8 +89,4 @@ function startCountdown() {
   }, 1000);
 }
 
-window.addEventListener('load', function () {
-  document.body.style.opacity = 1;
-});
-
-feather.replace();
+feather.replace()
